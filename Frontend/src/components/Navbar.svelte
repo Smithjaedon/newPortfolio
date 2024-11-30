dae7ed8471ec3ab5faaf9005d9e6a9466bc9061a
Jaedon Smith <86936528+Smithjaedon@users.noreply.github.com>
2024-11-29 21:06:16 -0500

    fixed navbar responsiveness

--- Frontend/src/components/Navbar.svelte
+++ Frontend/src/components/Navbar.svelte
@@ -1,10 +1,52 @@
-<header class="w-screen sticky top-0 bg-slate-800 text-slate-300 px-4">
-	<nav class="flex justify-between items-center p-4 w-full">
+<script>
+	let menuOpen = $state(false);
+
+	const toggleMenu = () => {
+		menuOpen = !menuOpen;
+	};
+
+	const closeMenu = () => {
+		menuOpen = false;
+	};
+</script>
+
+<header class="sticky top-0 max-h-16 w-screen bg-slate-800 px-4 text-slate-300">
+	<nav class="flex w-full items-center justify-between p-4">
 		<span>Jaedon Smith</span>
-		<ul class="md:flex gap-4 list-none hidden">
-			<li><a class="uppercase hover:text-slate-600" href="#about">About</a></li>
-			<li><a class="uppercase hover:text-slate-600" href="#projects">Projects</a></li>
+		<ul class="menu hidden list-none gap-4 md:flex" class:menu_open={menuOpen ? true : false}>
+			<li>
+				<a onclick={() => closeMenu()} class="uppercase hover:text-slate-600" href="#about">About</a
+				>
+			</li>
+			<li>
+				<a onclick={() => closeMenu()} class="uppercase hover:text-slate-600" href="#projects"
+					>Projects</a
+				>
+			</li>
 		</ul>
-		<button class="text-3xl md:hidden">☰</button>
+		<button class="text-3xl md:hidden" onclick={() => toggleMenu()}>☰</button>
 	</nav>
 </header>
+
+<style>
+	.menu_open {
+		display: flex;
+		flex-direction: column;
+		position: absolute;
+		top: 60px;
+		left: 0;
+		right: 0;
+		background-color: #1e293b;
+		z-index: 1000;
+		text-align: center;
+		padding-bottom: 10px;
+	}
+
+	/* ul {
+		flex-direction: column;
+		gap: 10px;
+		padding: 10px;
+		width: 100%;
+		text-align: center;
+	} */
+</style>
